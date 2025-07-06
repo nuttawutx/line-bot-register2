e0b12b6a23666830d7a1713f5f967f9045767465
nuttawutx <hr.recruitn@gmail.com>
2025-07-06 20:16:05 +0700

    update: ทดสอบรันรหัส

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def handle_message(event):
             default_code = "P"+60000
         elif emp_type == "รายเดือน":
             worksheet = client.open("HR_EmployeeList").worksheet("MonthlyEmployee")
-            default_code = 60000
+            default_code = 20000
         else:
             line_bot_api.reply_message(
                 event.reply_token,
